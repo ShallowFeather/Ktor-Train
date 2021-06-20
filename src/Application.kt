c75c21a9f0b92d1d36850925bf9fe0e3fc9bee48
ShallowFeather <74778927+ShallowFeather@users.noreply.github.com>
2021-06-21 04:47:16 +0800

    Initial commit

---
+++ src/Application.kt
@@ -0,0 +1,41 @@
+package com.example
+
+import io.ktor.application.*
+import io.ktor.response.*
+import io.ktor.request.*
+import io.ktor.features.*
+import io.ktor.routing.*
+import io.ktor.http.*
+import io.ktor.auth.*
+import com.fasterxml.jackson.databind.*
+import io.ktor.jackson.*
+import io.ktor.client.*
+import io.ktor.client.engine.apache.*
+import io.ktor.client.engine.jetty.*
+import java.util.*
+
+fun main(args: Array<String>): Unit = io.ktor.server.netty.EngineMain.main(args)
+
+data class Snippet(val text: String)
+
+data class User(
+    var Name: String,
+    var Password: String
+)
+
+
+fun Application.module() {
+    install(ContentNegotiation) {
+        jackson {
+            enable(SerializationFeature.INDENT_OUTPUT) // 美化输出 JSON
+        }
+    }
+    routing {
+        post ("/login"){
+            val userinput = call.receive<User>()
+            if (userinput.Name.length < 6 || userinput.Password.length < 8) {
+
+            }
+        }
+    }
+}
